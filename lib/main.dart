d6be07fea5a4f347f33fd32eb20cd6d4ce0a4c3d
Patrycja Koscielniak <patrycja.koscielniak.eu@gmail.com>
2023-05-01 17:36:25 +0200

    Add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:my_bullet_journal/firebase_options.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'auth/auth_gate.dart';
 
@@ -40,6 +39,10 @@ class MyApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(
           centerTitle: true,
         ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            backgroundColor: Colors.white,
+            unselectedItemColor: Colors.blueGrey,
+            selectedItemColor: Color.fromARGB(255, 160, 117, 217)),
       ),
       home: const AuthGate(),
     );
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -3,30 +3,78 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:my_bullet_journal/auth/user_profile.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({
+class HomePage extends StatefulWidget {
+  const HomePage({
     Key? key,
     required this.currentUser,
   }) : super(key: key);
 
   final User currentUser;
 
-  final usersQuery =
-      FirebaseFirestore.instance.collection('users').orderBy('displayName');
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 4;
   @override
   Widget build(BuildContext context) {
     //debugPrint(AppLocalizations.of(context)!.helloWorld):
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Vision Board'),
+        title: const Text('Feature Name'),
         actions: [
           IconButton(
               onPressed: () {
                 Navigator.of(context).push(MaterialPageRoute(
                     builder: ((context) => const UserProfileScreen())));
               },
-              icon: const Icon(Icons.person))
+              icon: const Icon(Icons.settings))
+        ],
+      ),
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return const Text('Log');
+        }
+        if (currentIndex == 1) {
+          return const Text('Wishlist');
+        }
+        if (currentIndex == 2) {
+          return const Text('Calendar');
+        }
+        if (currentIndex == 3) {
+          return const Text('HabitTracker');
+        }
+        return const Text('VisionBoard');
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book_outlined),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star_border),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.task_alt),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_view),
+            label: '',
+          ),
         ],
       ),
     );
