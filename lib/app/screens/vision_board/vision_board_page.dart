d2a8d97bb8a5db93d5b0c33eef8aa0a8691799a6
Patrycja Koscielniak <patrycja.koscielniak.eu@gmail.com>
2023-05-19 22:17:31 +0200

    Reorder vision_board logic

--- lib/app/screens/vision_board/vision_board_page.dart
+++ lib/app/screens/vision_board/vision_board_page.dart
@@ -21,10 +21,9 @@ class _VisionBoardState extends State<VisionBoard> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => VisionBoardCubit(VisionBoardRepository())..start(),
-      child: BlocListener<VisionBoardCubit, VisionBoardState>(
-        listener: (context, state) {},
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => VisionBoardCubit(VisionBoardRepository()),
         child: BlocBuilder<VisionBoardCubit, VisionBoardState>(
           builder: (context, state) {
             final images = state.items;
@@ -33,60 +32,57 @@ class _VisionBoardState extends State<VisionBoard> {
               allImages.add(image.image);
             }
             if (allImages.isEmpty) {
-              return const Scaffold(
-                  body: Center(
+              return const Center(
                 child: Text('Create your Vision Board'),
-              ));
+              );
             }
-            return Scaffold(
-              body: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: StaggeredGridView.countBuilder(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 12,
-                  itemCount: allImages.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      decoration: const BoxDecoration(
-                          color: Colors.transparent,
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(15),
-                          )),
-                      child: ClipRRect(
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(15),
-                          ),
-                          child: Image.network(
-                            allImages[index],
-                            fit: BoxFit.cover,
-                          )),
-                    );
-                  },
-                  staggeredTileBuilder: (index) {
-                    return StaggeredTile.count(1, index.isEven ? 1.2 : 1.8);
-                  },
-                ),
-              ),
-              floatingActionButton: FloatingActionButton(
-                onPressed: () async {
-                  XFile? pickedFile = await ImagePicker()
-                      .pickImage(source: ImageSource.gallery);
-                  setState(() {
-                    pickedImage = pickedFile;
-                    if (pickedImage != null) {
-                      context.read<VisionBoardCubit>().addImage(pickedImage!);
-                    } else {
-                      return;
-                    }
-                  });
+            return Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: StaggeredGridView.countBuilder(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 12,
+                itemCount: allImages.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        )),
+                    child: ClipRRect(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                        child: Image.network(
+                          allImages[index],
+                          fit: BoxFit.cover,
+                        )),
+                  );
                 },
-                mini: true,
-                child: const Icon(Icons.add_a_photo),
+                staggeredTileBuilder: (index) {
+                  return StaggeredTile.count(1, index.isEven ? 1.2 : 1.8);
+                },
               ),
             );
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          XFile? pickedFile =
+              await ImagePicker().pickImage(source: ImageSource.gallery);
+          setState(() {
+            pickedImage = pickedFile;
+            if (pickedImage != null) {
+              context.read<VisionBoardCubit>().addImage(pickedImage!);
+            } else {
+              return;
+            }
+          });
+        },
+        mini: true,
+        child: const Icon(Icons.add_a_photo),
       ),
     );
   }
